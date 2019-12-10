2608b2e1978f4711e7472eb730f8d756b1098307
Roy <18832880832@163.com>
2019-12-10 16:23:07 +0800

    完善项目

---
+++ src/main/java/com/in/f/sackcloth/SackclothApplication.java
@@ -0,0 +1,13 @@
+package com.in.f.sackcloth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SackclothApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SackclothApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/in/f/sackcloth/SackclothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.f.sackcloth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SackclothApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
